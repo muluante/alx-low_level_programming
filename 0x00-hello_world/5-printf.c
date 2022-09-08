20c3e9ad183225be09e5ccb21549010cf197bf8d
muluante <muluante@outlook.com>
2022-09-08 07:43:39 -0700

    5. Hello Prinf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - print the string in the printf 
+ * main -print the string in the printf 
  * description: using main function
  * it pints "with proper grammar, but the outcome is a piece of art,"
  * Return:Zero
