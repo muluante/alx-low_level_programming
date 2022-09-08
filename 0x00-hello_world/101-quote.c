f7ee1ea49d9091243aec7d3b0de2a1a3d20f1a3a
muluante <muluante@outlook.com>
2022-09-08 10:47:00 -0700

    8.qoute

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+
+/**
+ * main - print a text
+ * Return: always 1 (success)
+ */
+int main(void)
+{
+	write(2,"and that piece of art is useful\" - DoraKorpar,2015-10-19\n", 59);
+	return (1);
+}
