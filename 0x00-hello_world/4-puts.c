e8eab3f63a74485232bdc632fcd3c352a3dbcb0e
muluante <muluante@outlook.com>
2022-09-08 06:44:42 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
-/* using the function put
+
+/**
+ *  using the function put
+ *
  * return the value zero
- * print "Programming is like building a multilingual puzzle"
+ * prints "Programming is like building a multilingual puzzle"
  */
 int main(void)
 {
-	puts("\n" " Programming is like building a multilingual puzzle");
+	puts("\n" Programming is like building a multilingual puzzle");
 	return (0);
 }
