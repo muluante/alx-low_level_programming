c722d46df4428e95f88d0e0e7c1b5f2bb7d29771
muluante <muluante@outlook.com>
2022-09-09 08:52:39 -0700

    6. print numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
-
- /**
-  * main - Entry point
-  * Return: always zero (Seccess/Correct)
-  */
-
+/**
+ * main - Entry point
+ * Return: always zero (seccess/correct)
+ */
 int main(void)
-
 {
-
 	int i;
 
 	for (i = 48; i <= 57; i++)
