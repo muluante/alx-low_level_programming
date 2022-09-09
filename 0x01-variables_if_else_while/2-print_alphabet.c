b7e64e457a91e1656ac211a465402fb84009d9d7
muluante <muluante@outlook.com>
2022-09-09 06:18:54 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: always zero (Seccess/Correct)
+ */
+
+int main(void)
+
+{
+
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+	putchar (alphabet);
+}
+
+putchar('\n');
+
+return (0);
+
+}
