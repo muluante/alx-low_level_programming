5bace9428037aa9f8d8582842913ea1461f025fd
muluante <muluante@outlook.com>
2022-09-09 07:49:44 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+ /**
+  * main - entry point
+  * Return: always zero (Seccess/Correct)
+  */
+
+int main(void)
+
+{
+
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar (i);
+	}
+
+	putchar ('\n');
+
+	return (0);
+
+}
