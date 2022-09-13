9a793e19205bf98de92fb87eeb52461af532be9d
muluante <muluante@outlook.com>
2022-09-13 08:02:53 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
- * main - print alphabet function print
+ * print_alphabet - function print
  * Return: zero
 */
 void print_alphabet(void)
 {
 	char c;
 
-	for (c = 'a' ; c <= 'z'; c++)
+	for  (c = 'a'; c <= 'z'; c++)
 	{
-	_putchar(c);
-  	}	
+		_putchar(c);
+	}
 	_putchar('\n');
+
 }
