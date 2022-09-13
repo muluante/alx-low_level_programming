91a7e850f5c1a6cd5f43f90dcd3136ffb7c45953
muluante <muluante@outlook.com>
2022-09-13 11:24:46 -0700

    6.abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs - function that computes the absolute value
+ * @c: is the int 
+ * Return:zero
+ */
+int _abs(int c)
+{
+	if (c > 0 || c == 0)
+	{
+		return (c);
+	}
+	else
+		return (c * -1);
+}
+
