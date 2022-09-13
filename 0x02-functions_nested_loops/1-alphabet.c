f67a2ff19df4fbbc40a5631ff1dce15cd4946f83
muluante <muluante@outlook.com>
2022-09-13 07:52:03 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - print alphabet function print
+ * Return: zero
+*/
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z'; c++)
+	{
+	_putchar(c);
+  	}	
+	_putchar('\n');
+}
