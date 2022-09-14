1795b108bbc62ef3293b3f3ade5e3ebf0117c5e3
muluante <muluante@outlook.com>
2022-09-14 03:26:48 -0700

    0. Multiple mains

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,5 +2,10 @@
 #define MAIN_H
 
 #include <stdio.h>
-int positive_or_negative(int i);
+
+void positive_or_negative(int i);
+int largest_number(int, int, int);
+void print_remaining_days(int);
+int convert_days(int, int);
+
 #endif
