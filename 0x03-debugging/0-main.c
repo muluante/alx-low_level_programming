f21ef9c25af7e915e57d1750093a376f5d58047b
muluante <muluante@outlook.com>
2022-09-14 03:06:46 -0700

    0. Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - test function that prints
+ * Return: zero
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	posetive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+void posetive_or_negative(int i);
+#endif
