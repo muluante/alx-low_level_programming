23bef31a2354a9006a646c731ec6f6e0f724cdd3
muluante <muluante@outlook.com>
2022-09-14 08:20:20 -0700

    print_remaining_days

--- 0x03-debugging/3-main_a.c
+++ 0x03-debugging/3-main_a.c
@@ -18,7 +18,7 @@ int main(void)
 	year = 1997;
 	printf("Date: %02d/%02d/%04d\n", month, day, year);
 
-	day = convert_day(month, day);
+	day = convert_days(month, day);
 	print_remaining_days(month, day, year);
 
 	return (0);
