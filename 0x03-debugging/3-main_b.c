2f06dabd66122dc418123ec2ab713f1f3343e13c
muluante <muluante@outlook.com>
2022-09-14 09:01:38 -0700

    3. leap year

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -18,7 +18,7 @@ int main(void)
 	year = 2000;
 
 	printf("Date: %02d/%02d/%04d\n", month, day, year);
-	printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+	printf("Invalid date: %02d/%02d/%04d\n", month, day , year);
 
 	day = convert_day(month, day);
 
