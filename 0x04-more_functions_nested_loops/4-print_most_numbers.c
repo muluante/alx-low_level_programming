994fe822c59b24451036580d5d7f8cd7a5eee5eb
muluante <muluante@outlook.com>
2022-09-15 09:01:39 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers except 2 and 4
+ * Return: returns nothing
+ */
+
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+	{
+		if ((num == 50) || (num == 52))
+		{
+			continue;
+		}
+		putchar(num);
+	}
+	putchar(num);
+}
