f7408019dc3b4eaec8f5004e31b0f48b83557c78
muluante <muluante@outlook.com>
2022-09-19 03:31:30 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#define MAIN_H
+#include <stdio.h>
+
+void reset_to_98(int n);
+void swap_int(int *a, int *b);
+int _strlen(char *5);
+void _puts(char *str);
+void _putchar(char c);
+void print_rev(char *5);
+void puts2(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+void rev_string(char *3);
+#endif /* MAIN_H */
