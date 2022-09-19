14c107d5689668c0bc7b61fe321ed3f32cc51620
muluante <muluante@outlook.com>
2022-09-19 07:20:31 -0700

    6. Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - prints character in string
+ * @str: pointer to string
+ * Return: nothing
+ */
+
+void puts2(char *str)
+{
+	int a, b;
+
+	a = 0;
+
+	while (str[a] != '\0')
+	{
+		if (a % 2 == 0)
+		{
+			_putchar(str[a]);
+		}
+		a++;
+	}
+	_putchar('\n');
+}
