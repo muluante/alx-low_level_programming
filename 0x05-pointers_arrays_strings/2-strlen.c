508768e7088ff8f42f6ebc2a22e52a8942a23e08
muluante <muluante@outlook.com>
2022-09-19 04:14:15 -0700

    2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - function that return the length of string
+ * @s: the input
+ * Return: string length
+ */
+
+int _strlen(char *s)
+{
+	int length;
+
+	for (length = 0; s[length] != '\0'; length++)
+
+		return (length);
+}
