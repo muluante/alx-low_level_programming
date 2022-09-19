704958996ccfef2e8c5461625ce0171a884a3cc1
muluante <muluante@outlook.com>
2022-09-19 05:42:40 -0700

    . I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - a function for puts
+ * @str: printed string
+ * Return: zero
+ */
+
+void _puts(char *str)
+{
+	int length;
+
+	for (length = 0; str[length] != '\0'; length++)
+		_putchar(str[length]);
+
+	_putchar('\n);
+
+}
