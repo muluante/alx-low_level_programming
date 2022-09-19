c40d235e6c77943a367c705d87c4d8b8c599331c
muluante <muluante@outlook.com>
2022-09-19 03:53:09 -0700

    0. 98 Battery st.

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,14 +1,15 @@
+#ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
 
-void reset_to_98(int n);
+void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-int _strlen(char *5);
+int _strlen(char *s);
 void _puts(char *str);
-void _putchar(char c);
-void print_rev(char *5);
+void print_rev(char *s);
+void rev_string(char *s);
 void puts2(char *str);
+void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-void rev_string(char *3);
 #endif /* MAIN_H */
