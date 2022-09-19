218224c04f85c3140dc24329f1b2e8750201ceb4
muluante <muluante@outlook.com>
2022-09-19 08:56:27 -0700

    11. Don't hate the hacker, hate the code

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - password generator
+ * Return: zero
+ */
+
+int main(void)
+{
+	int a, b;
+
+	srand(time(NULL))
+		b = 0;
+	while (b <= 2645)
+	{
+		a = (rand() % 128);
+		b += a;
+		printf("%c", a);
+	}
+	printf("%c", 2772 - a);
+
+	return (0);
+}
