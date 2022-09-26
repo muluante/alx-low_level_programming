d2e2db0b1edbd7587cdc1c65a32decd39b3f8c2a
muluante <muluante@outlook.com>
2022-09-26 06:58:59 -0700

    2-strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _strchr - check the 
- * @s: The array to be searched
- * @c: The character being searched
- * Return: Always 0.
+ * _strchr - string character
+ * @s: array to be searched
+ * @c: character being searched
+ * Return: nothing to return
  */
 char *_strchr(char *s, char c)
 {
