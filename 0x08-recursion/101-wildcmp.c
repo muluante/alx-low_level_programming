acc1555105e6f8fffcf4cdf73b8dd7d4bc41ff9a
muluante <muluante@outlook.com>
2022-09-28 07:07:32 -0700

    101-wildcmp

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * wildcmp - function that compares two strings
+ * @s1: the first input string1
+ * @s2: the second input string2
+ * Return: 1 if true, 0 if false
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+			return (wildcmp(s1, s2 + 1));
+		return (*s2 == '\0');
+	}
+	if (*s2 == '*')
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	else if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	return (0);
+}
