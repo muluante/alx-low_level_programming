427df587e62d850e6cc0e208cb8a72fd8c98a876
muluante <muluante@outlook.com>
2022-10-03 05:04:01 -0700

    1-strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* _strdup - string to be copied
+* @str: string copied
+* Return: string
+*/
+
+char *_strdup(char *str)
+{
+	int a = 0, b, c = 0;
+	char *Strnew = NULL;
+
+	while (str[k] != '\0')
+	{
+		a++;
+		c++;
+	}
+
+	Strnew = malloc(sizeof(char) * (a + 1));
+
+	if (str == NULL)
+		return (NULL);
+
+	for (b = 0; b <= a; b++)
+		Strnew[b] = str[b];
+	return (Strnew);
+}
