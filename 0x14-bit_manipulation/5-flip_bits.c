7181725ddcbf5ac89c189e66e043c0cbe49aa8e3
muluante <muluante@outlook.com>
2022-10-27 01:52:17 -0700

    5-flip_bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * flip_bits - returns number of bits
+ * @n: value 1
+ * @m: value 2
+ *
+ * Return: number of bits to flip
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int flipbit;
+	unsigned long int xorop;
+
+	xorop = n ^ m;
+
+	flipbit = 0;
+
+	while (xorop != 0)
+	{
+		if ((xorop & 1) == 1)
+		{
+			flipbit++;
+		}
+		xorop = xorop >> 1;
+	}
+	return (flipbit);
+}
