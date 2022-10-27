339af4830dfcf820bfa7230f0e3d3439869931b5
muluante <muluante@outlook.com>
2022-10-27 04:01:55 -0700

    1-print_binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,16 +1,20 @@
-#include "mainn.h"
-/**
- * print_binary - prints the binary of a number
- * @n: a string of 0 and 1 chars
- *
- */
-void print_binary(unsigned long int n)
-{
-	int printbin = ((n & 1) + '0');
+#include "main.h"
 
-	if (n > 1)
-	{
-		print_binary(n >> 1);
-	}
-	_putchar(printbin);
+/**
+ * get_bit - Gets the value of a bit at a given index.
+ * @n: The bit.
+ * @index: The index to get the value at - indices start at 0.
+ *
+ * Return: If an error occurs - -1.
+ *         Otherwise - The value of bit at index.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
+	if ((n & (1 << index)) == 0)
+		return (0);
+
+	return (1);
 }
