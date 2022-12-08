ac2223af1b0453d61d5e44cbdc0dc31dedab5cf1
muluante <muluante@outlook.com>
2022-12-08 01:20:21 -0800

    103-keygen

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -3,18 +3,18 @@
 #include <string.h>
 
 /**
- *main - Generates and prints passwords
- *@argc: The number of arguments supplied
- *@argv: AN array of pointers
+ * main - Generates and prints passwords
+ * @argc: The number of arguments supplied
+ * @argv: An array of pointers
  *
- *Return: Always 0
+ * Return: Always 0
  */
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	char password[7], *codex;
 	int len = strlen(argv[1]), i, tmp;
 
-	codex = "A-CHRDw871NS0E9BTibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+	codex = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 
 	tmp = (len ^ 59) & 63;
 	password[0] = codex[tmp];
@@ -27,7 +27,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	tmp = 1;
 	for (i = 0; i < len; i++)
 		tmp *= argv[1][i];
-	passowrd[2] = codex[(tmp ^ 85) & 63];
+	password[2] = codex[(tmp ^ 85) & 63];
 
 	tmp = 0;
 	for (i = 0; i < len; i++)
@@ -43,9 +43,9 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		tmp += (argv[1][i] * argv[1][i]);
 	password[4] = codex[(tmp ^ 239) & 63];
 
-	for ( i = 0; i < argv[1][0]; i++)
+	for (i = 0; i < argv[1][0]; i++)
 		tmp = rand();
-	password[5] = codex[tmp ^ 229) & 63];
+	password[5] = codex[(tmp ^ 229) & 63];
 
 	password[6] = '\0';
 	printf("%s", password);
