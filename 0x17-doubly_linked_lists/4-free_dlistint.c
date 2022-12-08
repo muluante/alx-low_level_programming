0e074369885329b18717587cd6b12c2a2a9731d3
muluante <muluante@outlook.com>
2022-12-08 00:39:04 -0800

    4-free_dlistint

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ *free_dlistint - Frees the linked
+ *@head: The head of the list
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
