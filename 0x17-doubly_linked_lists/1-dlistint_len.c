270463d1137b993dcdcc4942b480dc9d7d5615e8
muluante <muluante@outlook.com>
2022-12-08 00:31:55 -0800

    1-dlistint_len.c

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+
+/**
+ *dlistint_len - counts the number of elements
+ *@h: The head of the dlistint list
+ *
+ * Return: The number of elements in the list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
+}
